5d55d6e16d592cc3f34cf6808fdf635e5d130bce
hugocarvalho9 <hugocarvalho09@gmail.com>
2018-11-12 23:30:41 +0000

    tpc8

---
+++ TP8/myUpApp/public/javascripts/myUpApp.js
@@ -0,0 +1,48 @@
+$(() => {
+    $('#ficheiros').load('http://localhost:5001/fich')
+
+
+    $('#adicionar').click(e=> {
+        e.preventDefault()
+        var filename = $('#fich').val().split('\\').pop();
+        var url = '"/images/'+filename+'"'
+        $('#ficheiros').append('<tr><td>'+ '<a href='+url+'>'+filename+'</a>'+'</td>'+'<td>'+$('#desc').val()+'</td></tr>')
+        ajaxPost()
+        formPost()
+    })
+
+    function ajaxPost() {
+        $.ajax({
+            type:"POST",
+            contentType: "application/json",
+            url: "http://localhost:5001/fich/guardar",
+            data: JSON.stringify({ficheiro: $('#fich').val().split('\\').pop(),desc: $('#desc').val()}),
+            success: p => alert('Dados gravados com sucesso! -> '+p),
+            error: e => {
+                alert('Erro no post: ' + JSON.stringify(e))
+                console.log('ERRO: '+e)
+            }
+        })
+    }
+
+    function formPost() {
+        var form_data = new FormData($('#myUploadForm')[0]);
+        $.ajax({
+            type:'POST',
+            url:'http://localhost:5001/processa',
+            processData: false,
+            contentType: false,
+            async: true,
+            cache: false,
+            data : form_data,
+            timeout: 6000,
+            success: p => alert('Ficheiro gravado com sucesso! -> '+p),
+            error: e => {
+                alert('Erro no post: ' + JSON.stringify(e))
+                console.log('ERRO: '+e)
+            }       
+        })
+        $('#desc').val('')
+        $('#fich').val('')
+    }
+})
